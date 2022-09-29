15888cb121edeb6f57c9f9520925cb158a7bc2f0
Cade Courtney <cacourtney3@gmail.com>
2022-09-29 13:38:35 -0700

    painting works

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const grid = document.querySelector('.grid');
 
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
 function fillGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for(var i = 0; i < size * size; i++) {
         const ele = document.createElement('div');
-        ele.classList.add('grid-element');
+        ele.classList.add('grid-item');
         ele.textContent = i;
-        ele.addEventListener('mouseover', changeNum);
-        ele.addEventListener('mousedown', changeNum);
+        ele.addEventListener('mouseover', changeColor);
+        ele.addEventListener('mousedown', changeColor);
         grid.appendChild(ele);
     }
 }
 
 // click mousedown works. 
 function changeNum(e) {
-    if(e.type == 'mouseover' && e.type != 'mousedown') {
+    if(e.type == 'mouseover' && false != mouseDown) {
         return;
     }
-    // e.target.textContent = e.target.textContent * 2;
+    e.target.textContent = e.target.textContent * 2;
+}
+
+// click mousedown works. 
+function changeColor(e) {
+    if(e.type == 'mouseover' && !mouseDown) {
+        return;
+    }
+    e.target.style.backgroundColor = 'red';
 }
 
 window.onload = () => {
-    fillGrid(4)
+    fillGrid(4);
   }
